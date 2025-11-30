5fbcfb0570e472b34db2c093461dff858374d988
JaMya Hart <jamyaalexander2004@gmail.com>
2025-11-30 16:55:49 -0600

    Stage 4 Attempt 1

---
+++ blocks/index.js
@@ -0,0 +1,23 @@
+const editProfileBtn  = docuument.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editCloseBtn= editProfileModal.querySelector("modal__close-btn");
+
+const newPostBtn  = docuument.querySelector(".profile__add-btn");
+const newPostModal = document.querySelector("#new-profile-modal");
+const newCloseBtn= newPostModal.querySelector("modal__close-btn");
+
+editProfileBtn.addEventListener("click", function(){
+    editProfileModal.classList.add ("modal_is-opened");
+});
+
+editCloseBtn.addEventListener("click",function(){
+    editProfileModal.classList.add("modal_is-opened");
+});
+
+newPostBtn.addEventListener("click", function(){
+   newPostModal.classList.add ("modal_is-opened");
+});
+
+newCloseBtn.addEventListener("click",function(){
+    newPostModal.classList.add("modal_is-opened");
+});
