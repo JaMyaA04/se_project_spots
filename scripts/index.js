7a59591087d7e975264298c5c967e43250e9b6a0
JaMya Hart <jamyaalexander2004@gmail.com>
2025-12-24 23:01:11 -0600

    stage 5 attempt 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,31 @@
 const editProfileBtn  = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editCloseBtn= editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm= editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
 const newPostBtn  = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newCloseBtn= newPostModal.querySelector(".modal__close-btn");
+const newPostForm= newPostModal.querySelector(".modal__form");
+const newPostImageInput=newPostModal.querySelector("#card-image-input");
+const newPostCaptionInput=newPostModal.querySelector("#card-image-caption");
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
 editProfileBtn.addEventListener("click", function(){
+    editProfileNameInput.value = profileNameEl.textContent;
+    /*the text content of the [profileNameEl] will now be filled with the value of the [editProfileNameEl] element*/
+    editProfileDescriptionInput.value = profileDescriptionEl.textContent;
     editProfileModal.classList.add ("modal_is-opened");
 });
 
 editCloseBtn.addEventListener("click",function(){
     editProfileModal.classList.remove("modal_is-opened");
+   
+
 });
 
 newPostBtn.addEventListener("click", function(){
@@ -21,3 +35,20 @@ newPostBtn.addEventListener("click", function(){
 newCloseBtn.addEventListener("click",function(){
     newPostModal.classList.remove("modal_is-opened");
 });
+function handleEditProfileSubmit(evt){
+    evt.preventDefault();
+    profileNameEl.textContent=editProfileNameInput.value ;
+   profileDescriptionEl.textContent=editProfileDescriptionInput.value; 
+   editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("sumbit",handleEditProfileSubmit);
+
+function handleNewPostSubmit(evt){
+    evt.preventDefault();
+    console.log(newPostImageInput);
+    console.log(newPostCaptionInput);
+    newPostModal.classList.remove("modal_is-opened");
+}
+
+newPostForm.addEventListener("submit",handleNewPostSubmit);
