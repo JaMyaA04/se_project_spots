56e4e1e049bbeb47d89292c9b2211b2b7c19deb8
JaMya Hart <jamyaalexander2004@gmail.com>
2026-03-06 03:58:31 -0600

    stage 6 attempt 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const initialcard = [
+const initialcards = [
     { name: "Val Thorens", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg" },
     { name : "Restaurant terrace", link : "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg" },
     { name : "An outdoor cafe", link : "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg" },
@@ -6,7 +6,7 @@ const initialcard = [
     { name : "Tunnel with morning light", link : "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg" },
     { name : "Mountain house", link : "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg" }];
 
-const modal = document.querySelectorAll(".modal")
+const modal = document.querySelectorAll(".modal");
 
 const editProfileBtn  = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
@@ -71,7 +71,7 @@ function handleNewPostSubmit(evt){
 
 newPostForm.addEventListener("submit", handleNewPostSubmit);
 
-initialcard.forEach(function (item) {
+initialcards.forEach(function (item) {
     console.log(item.name);
     console.log(item.link);
 })
